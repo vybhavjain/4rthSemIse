f63a64b64ce8ab2ff996da3d3609204759b23914
vybhavjain <35528017+vybhavjain@users.noreply.github.com>
2018-03-25 13:38:37 +0530

    Update binary search.asm

--- Microprocessors -X86-/binary search.asm
+++ Microprocessors -X86-/binary search.asm
@@ -1,11 +1,11 @@
 assume cs:code,ds:data
 data segment
-	a db 10h,20h,30h,40h,50h	; Sorting only bytes. Try for words also.
+	a db 10h,20h,30h,40h,50h	; Sorting only bytes.
 	n db n-a
 	key db 20h
 	msg1 db "key not found$"
 	msg2 db "key found at position: "
-	pos db ?,"$"			; msg2 continues till here!!!
+	pos db ?,"$"			; $ indicates till where message is supposed to be printed
 data ends
 
 code segment
@@ -13,24 +13,24 @@ start:
         mov ax,data
         mov ds,ax
 
-        mov al,0				; low
+        mov al,0			; low
         mov dl,n			
         dec dl				; high = n-1
 again:
         cmp al,dl
         ja failed
         mov cl,al
-        add al,dl				; low + high	
-        shr al,1	; mid	
+        add al,dl			; low + high	
+        shr al,1			; mid	
         mov ah,00h
         mov si,ax
         mov bl,[si]			; [mid] in bl
         cmp bl,key
-        jae loc1				; [mid]  >=  key ?
+        jae loc				; [mid]  >=  key ?
         inc al				; no, low = mid+1, to search in second half
         jmp again
 loc1:
-        je success				; [mid] = key ?
+        je success			; [mid] = key ?
         dec al				; no, high = mid – 1, to search in first half
         mov dl,al
         mov al,cl
@@ -40,8 +40,7 @@ failed:
         jmp display
 success:
         inc al
-        add al,30h			; store ASCII value at pos. guess why ????
-        mov pos,al
+        add al,30h			; store ASCII value at pos.
         lea dx,msg2
 
 display:
