eca54f48ec0faf84e735a9b06d0b5e09187c1dbb
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-04-20 07:38:53 +0530

    Create heapsort.c

---
+++ DAA LAB/heapsort.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<time.h>
+#include<math.h>
+int h[50];
+void heapify(int h[],int n)
+{
+ int i,j,n1,k,heap,v;
+ n1=n;
+ for(i=n/2;i>=1;i--)
+ {
+  k=i;
+  v=h[k];
+  heap=0;
+  while(!(heap)&&(2*k)<=n1)
+  {
+   j=2*k;
+   if(j<n1)
+    if(h[j]<h[j++])
+      j=j+1;
+   if(v>=h[j])
+       heap=1;
+   else 
+   {
+    h[k]=h[j];
+    k=j;
+   }
+  }
+h[k]=v;
+ }
+}
+
+
+void main()
+{
+ int i,n,size,t;
+ printf("Enter the value of n \n");
+ scanf("%d",&n);
+ printf("Enter the array elements \n");
+ for(i=1;i<=n;i++)
+  scanf("%d",&h[i]);
+  heapify(h,n);
+  printf("Elements heapified \n");
+  for(i=1;i<=n;i++)
+   printf("%d \t",h[i]);
+  for(i=n;i>=1;i--)
+  {
+   t=h[1];
+   h[1]=h[i];
+   h[i]=t;
+   size=i-1;
+    heapify(h,size);
+  }
+ printf("ROtated elements are: \n");
+ for(i=1;i<=n;i++)
+  printf("%d \t",h[i]);
+}
