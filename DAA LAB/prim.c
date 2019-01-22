73b3b831f74ec3a4cc3aeaef83b70d072c00a908
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-01-22 21:43:44 +0530

    Update prim.c

--- DAA LAB/prim.c
+++ DAA LAB/prim.c
@@ -1,43 +1,39 @@
 #include<stdio.h>
-
+int a,b,u,v,n,i,j,ne=1,source;
+int visited[20]={0},min,mincost=0,cost[20][20];
 void main()
-{ 
-  int n,i,j,min,ne=1,min_cost=0,cost[n][n],a,u,b,v,source,visited[20];
-  printf("number of vertices : \n ");
-  scanf("%d",&n);
-  printf("enter the cost matrix \n");
-  for(i=1;i<=n;i++)
-   for(j=1;j<=n;j++)
+{
+ printf("Enter the number of nodes\n");
+ scanf("%d",&n);
+ printf("Enter the cost matrix\n");
+ for(i=1;i<=n;i++)
+  for(j=1;j<=n;j++)
+  {
    scanf("%d",&cost[i][j]);
-  for(i=1;i<=n;i++)
-  visited[i]=0;
-  printf("enter source \n ");
-  scanf("%d",&source);
-  visited[source]=1;
-  while(ne<n)
-  { 
-    min=111;
-    for(i=1;i<=n;i++)
-     for(j=1;j<=n;j++)
-     {
-       if(cost[i][j]<min)
-         if(visited[i]==0)
- 	 continue;
-	 else
-	 {
-	   min=cost[i][j];
-	   a=u=i;
-	   b=v=j;
-	 }
-     }
-   if(visited[u]==0||visited[v]==0)
-   { 
-     printf(" \n edge %d \t %d -> %d =%d \n",ne++,a,b,min);
-     min_cost+=min;
-     visited[b]=1;
-   }   
-   cost[a][b]=cost[b][a]=999;
+   if(cost[i][j]==0)
+    cost[i][j]=999;
   }
-   printf("min cost is %d \n ",min_cost);
+ printf("Enter the source node\n");
+ scanf("%d",&source);
+ visited[source]=1;
+ while(ne<n)
+ {
+  for(i=1,min=999;i<=n;i++)
+   for(j=1;j<=n;j++)
+    if(cost[i][j]<min)
+     if(visited[i]!=0)
+     {
+      min=cost[i][j];
+      a=u=i;
+      b=v=j;
+     }
+  if(visited[u]==0 || visited[v]==0)
+  {
+   printf("Edge %d:(%d->%d) cost:%d\n",ne++,a,b,min);
+   mincost+=min;
+   visited[b]=1;
+  }
+  cost[a][b]=cost[b][a]=999;
+ }
+ printf("Minimun cost=%d\n",mincost);
 }
-
